897b01f42e31f6abb3fc70e2104925dc5c2ccd45
raimundlima <77973887+raimundlima@users.noreply.github.com>
2021-10-06 10:51:55 -0300

    remove list....

--- src/list-of-games/dto/update-list-of-game.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateListOfGameDto } from './create-list-of-game.dto';
-
-export class UpdateListOfGameDto extends PartialType(CreateListOfGameDto) {}
--- src/list-of-games/entities/list-of-game.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma } from ".prisma/client";
-
-export class ListOfGames implements Prisma.ListOfGamesUncheckedCreateInput{
-    id?:         number; 
-    userId:      number;
-    gameId:      number;
-}
